465ec9a234bef1534b2e048a2d2920a7b9829d67
KIM HYUN GI <rlagusrl928@gmail.com>
2023-09-05 00:03:41 +0900

    create-repository

---
+++ src/test/java/com/batch/pt/persnertrainingbatch/PersnerTrainingBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batch.pt.persnertrainingbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersnerTrainingBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
