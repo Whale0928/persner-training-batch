cafba8a38d013f36e42d71c477f1f0646c7bab8f
KIM HYUN GI <rlagusrl928@gmail.com>
2023-09-11 23:03:49 +0900

    feat : Create BaseEntity

--- src/main/java/com/batch/pt/persnertrainingbatch/PersnerTrainingBatchApplication.java
+++ src/main/java/com/batch/pt/persnertrainingbatch/PersnerTrainingBatchApplication.java
@@ -3,7 +3,6 @@ package com.batch.pt.persnertrainingbatch;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.scope.context.ChunkContext;
